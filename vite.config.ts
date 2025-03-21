5d1312ab8b71a2aa0cef838693f14107add32b0e
Daniel Berninghoff <daniel.berninghoff@tngtech.com>
2025-03-21 16:58:13 +0100

    serve manifest with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,33 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mobile Timesheet Web App',
+        short_name: 'Mobile Timesheet',
+        description: 'Web Application for mobile time tracking and exporting to timesheet',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }})
   ],
   base: "./",
   test: {
