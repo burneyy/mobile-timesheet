dd1ff0ba73f9bfe246f273ff98f2ccaa66ebeb17
Daniel Berninghoff <daniel.berninghoff@tngtech.com>
2025-03-21 15:37:02 +0100

    Updates

--- assets/md.transition-legacy-iwJwLmEe.js
+++ assets/md.transition-legacy-iwJwLmEe.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DsOOJkiZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-7VKThg4R.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
