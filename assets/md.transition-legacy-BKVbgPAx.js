dfacbde23bb4447d2577302641993f2b4514d62b
Daniel Berninghoff <daniel.berninghoff@tngtech.com>
2025-03-21 14:58:27 +0100

    Updates

--- assets/md.transition-legacy-BKVbgPAx.js
+++ assets/md.transition-legacy-BKVbgPAx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BOV0aEF5.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-AvKCAe8H.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
